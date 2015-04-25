29435325cd9cc359b45c2068ffb2db8ac0e6147a
leozdgao <gzdleo0816@gmail.com>
2015-04-25 23:28:34 +0800

    improve code

--- frontend/eventemmiter.js
+++ frontend/eventemmiter.js
@@ -3,35 +3,38 @@
 
 function EventEmitter () {
 
-	this.eventHandles = {};
+    this.eventHandles = {};
 }
 
 // add listener
 EventEmitter.prototype.on = function(event, callback) {
 
-	var handlers = this.eventHandles[event] || [];
-	handlers.push(callback);
+    var handlers = this.eventHandles[event] || [];
+    handlers.push(callback);
 
-	return handlers.length - 1;
+    return handlers.length - 1;
 };
 
 // fire event
 EventEmitter.prototype.emit = function(event) {
 
-	for(var i = 0; i < this.eventHandles.length; i++) {
+    if(this.eventHandles[event]) {
+        
+        for(var i = 0, l = this.eventHandles.length; i < l; i++) {
 
-		var args = Array.prototype.slice(1);
-		var handler = this.eventHandles[i];
-		
-		handler.apply(null, args);
-	}
+            var args = Array.prototype.slice(1);
+            var handler = this.eventHandles[i];
+            
+            handler.apply(null, args);
+        }   
+    }
 };
 
 // remove listener
 EventEmitter.prototype.remove = function(event, index) {
 
-	var handlers = this.eventHandles[event] || [];
+    var handlers = this.eventHandles[event] || [];
 
-	handlers.splice(index);
+    handlers.splice(index);
 };
 
